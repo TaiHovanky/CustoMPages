996cf5f8865b3cdb8154137311f143c98760ad5d
Tai Hovanky <thovanky@arrivelogistics.com>
2018-10-15 09:19:56 -0500

    'removing-client-id'

--- example-smart-app/src/index.js
+++ example-smart-app/src/index.js
@@ -21,8 +21,8 @@ class App extends React.Component {
 
     componentDidMount() {
         const demo = {
-            serviceUrl: "https://r2.smarthealthit.org",
-            patientId: "smart-1137192"
+            serviceUrl: "https://r2.smarthealthit.org"
+            // patientId: "smart-1137192"
         };
         console.log('demo-----', demo);
         var smart = FHIR.client(demo),
