6c0421fbb39d9926a603348af4734817caad48c8
Tai Hovanky <tai.hovanky@gmail.com>
2018-10-16 22:28:02 -0500

    'address'

--- example-smart-app/src/components/patient-contact-card/index.js
+++ example-smart-app/src/components/patient-contact-card/index.js
@@ -13,7 +13,7 @@ const PatientContactCard = ({ address, telecom }) => (
         <CardTitle title="Contact Info" />
         <CardText>
             <List>
-                <ListItem primaryText={address && address.length > 1 ? address[0].text : '' } />
+                <ListItem primaryText={address && address.length > 0 ? `${address[0].text}` : '' } />
                 {telecom.map(contact => (
                     <ListItem primaryText={`${contact.system} ${contact.value}`} />
                 ))}
